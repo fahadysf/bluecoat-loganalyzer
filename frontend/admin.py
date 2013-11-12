4d981448cf3a8fe6c5b95af3aac9574f8c0195eb
Fahad Yousuf <fahadysf@gmail.com>
2013-11-12 11:36:46 +0300

    Enabling admin

--- frontend/admin.py
+++ frontend/admin.py
@@ -5,7 +5,16 @@ from frontend.models import UserLog, IPLog, PermanentLimitExceptions
 
 
 class UserLogAdmin(admin.ModelAdmin):
-    list_display = ('date','data_usage','__str__')
+    list_display = ('username','date','data_usage')
+
+    def username(self, obj):
+        return obj.username
+
+    def date(self, obj):
+        return str(obj.date)
+
+    def data_usage(self, obj):
+        return str(obj.data_usage)
 
 class IPLogAdmin(admin.ModelAdmin):
     list_display = ('ip_addr','date','data_usage')
