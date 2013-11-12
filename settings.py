aaf4d0cf63dc94fe1b4937e6d61b2479d6cac242
Fahad Yousuf <fahadysf@gmail.com>
2013-11-12 17:26:25 +0300

    Enabling admin

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-SITE_ID = 1
+SITE_ID = u'5281db0ecd33b20dfaa02e2d'
+
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -13,12 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bluecoatstatsdb',     # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'sql000sql',                    # Not used with sqlite3.
-        'HOST': '127.0.0.1',                        # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                                 # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'bluecoat-stats-db',     # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
