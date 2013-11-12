ae9e64069c44c3d6fb2bf437472abb934afc826e
Fahad Yousuf <fahadysf@gmail.com>
2013-11-12 10:41:39 +0300

    Enabling admin

--- settings.py
+++ settings.py
@@ -122,7 +122,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cas_provider',
     'frontend',
 	# Uncomment the next line to enable the admin:
     'django.contrib.admin',
