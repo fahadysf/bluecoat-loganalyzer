e1be8a7c7b63ed6fcfce0acd0940ba3cdcaa0587
Fahad Yousuf <fahadysf@gmail.com>
2013-11-13 10:23:47 +0300

    Fixing blocking CPL

--- frontend/admin.py
+++ frontend/admin.py
@@ -20,7 +20,7 @@ class DailyExceptionsAdmin(admin.ModelAdmin):
     search_fields = ('ip_addr','username')
 
 class PermanentLimitExceptionsAdmin(admin.ModelAdmin):
-    list_display = ('date','username','ip_addr','data_limit')
+    list_display = ('username','ip_addr','data_limit')
     list_filter = ('date','data_limit')
     search_fields = ('ip_addr','username')
 
