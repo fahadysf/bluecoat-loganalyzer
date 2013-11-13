3bb5ad1be700ec605019bb0e988a886d832cb69b
Fahad Yousuf <fahadysf@gmail.com>
2013-11-13 10:18:49 +0300

    Fixing blocking CPL

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,22 +1,31 @@
 __author__ = 'Fahad'
 
 from django.contrib import admin
-from frontend.models import UserLog, IPLog, PermanentLimitExceptions
+from frontend.models import UserLog, IPLog, DailyExceptions, PermanentLimitExceptions
 
 
 class UserLogAdmin(admin.ModelAdmin):
-    list_display = ('username','date','data_usage','deny_count','custom_limit','blocked')
-    list_filter = ('date','blocked')
+    list_display = ('username','date','data_usage','deny_count','blocked')
+    list_filter = ('data_usage','blocked')
     search_fields = ('username',)
 
 class IPLogAdmin(admin.ModelAdmin):
-    list_display = ('ip_addr','date','data_usage','deny_count','custom_limit','blocked')
-    list_filter = ('date','blocked')
+    list_display = ('ip_addr','date','data_usage','deny_count','blocked')
+    list_filter = ('data_usage','blocked')
     search_fields = ('ip_addr',)
 
+class DailyExceptionsAdmin(admin.ModelAdmin):
+    list_display = ('date','username','ip_addr','data_limit'')
+    list_filter = ('date','data_limit')
+    search_fields = ('ip_addr','username')
+
+
 class PermanentLimitExceptionsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('date','username','ip_addr','data_limit'')
+    list_filter = ('date','data_limit')
+    search_fields = ('ip_addr','username')
 
 admin.site.register(UserLog, UserLogAdmin)
 admin.site.register(IPLog, IPLogAdmin)
+admin.site.register(DailyExceptions, DailyExceptionsAdmin)
 admin.site.register(PermanentLimitExceptions, PermanentLimitExceptionsAdmin)
