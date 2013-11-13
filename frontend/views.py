713aaf5cb96a8f0cfe9951b77f9b8c9c9cb6fde5
Fahad Yousuf <fahadysf@gmail.com>
2013-11-14 00:18:44 +0300

    Putting daily exception at higher priority than permanent exception.

--- frontend/views.py
+++ frontend/views.py
@@ -7,22 +7,22 @@ from django.shortcuts import render_to_response
 from frontend.models import UserLog, IPLog, DailyStatistics
 
 def bw_report_users(request):
-    userloglist_qs = UserLog.objects.filter(date=datetime.datetime.utcnow().date()).order_by('-data_usage')
+    userloglist_qs = UserLog.objects.filter(date=datetime.datetime.now().date()).order_by('-data_usage')
     context = {'userlogs': userloglist_qs[:100], }
     return render_to_response('top-users-bw.html', context)
 
 def bw_report_ips(request):
-    iploglist_qs = IPLog.objects.filter(date=datetime.datetime.utcnow().date()).order_by('-data_usage')[:100]
+    iploglist_qs = IPLog.objects.filter(date=datetime.datetime.now().date()).order_by('-data_usage')[:100]
     context = {'iplogs': iploglist_qs}
     return render_to_response('top-ips-bw.html', context)
 
 def deny_count_report_users(request):
-    userloglist_qs = UserLog.objects.filter(date=datetime.datetime.utcnow().date()).order_by('-deny_count')[:100]
+    userloglist_qs = UserLog.objects.filter(date=datetime.datetime.now().date()).order_by('-deny_count')[:100]
     context = {'userlogs': userloglist_qs}
     return render_to_response('top-users-deny-count.html', context)
     
 def deny_count_report_ips(request):
-    iploglist_qs = IPLog.objects.filter(date=datetime.datetime.utcnow().date()).order_by('-deny_count')[:100]
+    iploglist_qs = IPLog.objects.filter(date=datetime.datetime.now().date()).order_by('-deny_count')[:100]
     context = {'iplogs': iploglist_qs}
     return render_to_response('top-ips-deny-count.html', context)
 
