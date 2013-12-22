0c8cbf9763635812069f81f78f52658b2c8d04ac
Fahad Yousuf <fahadysf@gmail.com>
2013-12-22 13:11:35 +0300

    Adding date selector support

--- templates/static/js/custom.js
+++ templates/static/js/custom.js
@@ -304,10 +304,21 @@ function template_functions(){
 	$('.cleditor').cleditor();
 	
 	/* ---------- Datapicker ---------- */
-	$('.datepicker').datepicker({
+	function updateQueryStringParameter(uri, key, value) {
+      var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
+      separator = uri.indexOf('?') !== -1 ? "&" : "?";
+      if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+      }
+      else {
+        return uri + separator + key + "=" + value;
+      }
+    }
+
+    $('.datepicker').datepicker({
         dateFormat: "dd-mm-yy",
         onSelect: function(dateText, inst){
-            $(location).attr('href', location.href + '?date=' + dateText);
+            $(location).attr('href', updateQueryStringParameter(location.href, date, dateText);
         }
     });
 	
